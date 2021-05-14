ae5e8d3a12954a523e9eedb3139df72330213231
Peyton <gasiorowicz10@gmail.com>
2021-05-14 17:35:16 +0900

    Add pjt10

---
+++ pjt10/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <router-view />
+    <MovieList />
+  </div>
+</template>
+
+<script>
+import MovieList from "@/components/MovieList";
+
+export default {
+  data() {
+    return {};
+  },
+  components: {
+    MovieList
+  }
+};
+</script>
+
+<style>
+html,
+body {
+  height: 100%;
+  background: linear-gradient(to bottom right, #ff6b81, #8e44ad);
+}
+#app {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  max-width: 1000px;
+  box-shadow: 0 0 40px #333;
+}
+</style>
